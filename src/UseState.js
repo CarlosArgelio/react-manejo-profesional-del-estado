5f313bc7030614afc4eb26b6a70315b65b2d91a9
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-11-21 20:14:52 -0400

    feat: useEffect in componet UseState

--- src/UseState.js
+++ src/UseState.js
@@ -1,7 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function UseState({ name }) {
     const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (!!loading) {
+            setTimeout(() => {
+                setLoading(false);
+            }, 3000);
+        }
+    }, [loading]);
+
 
     return (
         <div>
@@ -12,10 +22,13 @@ function UseState({ name }) {
             {error && (
                 <p>Codigo de seguridad incorrecto</p>
             )}
+            {loading && (
+                <p>Cargando...</p>
+            )}
 
             <input  placeholder='Codeigo de seguridad'/>
             <button 
-                onClick={() => setError(!error)}
+                onClick={() => setLoading(true)}
             >Comprobar</button>
         </div>
     )
